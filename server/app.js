f6c844bed9ea3af6d9f2508d5408f0c8800bd0bf
Ana Carolina <anacgp91@gmail.com>
2017-05-11 21:22:14 -0300

    prontinho

--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
+"use strict"
+
 let express = require('express'),
     app = express();
-let fs = require('fs');    
+let fs = require('fs');
+let _ = require('underscore');
+
 
 // carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
 // você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
@@ -16,7 +20,7 @@ fs.readFile(__dirname + '/data/jogadores.json', 'utf-8', function(error, data) {
 	}
 });
 
-fs.readFile(__dirname + '/data/jogadores.json', 'utf-8', function(error, data) {
+fs.readFile(__dirname + '/data/jogosPorJogador.json', 'utf-8', function(error, data) {
 	if(error) {
 		console.log('ERRO: ' + error);
 	} else {
@@ -35,34 +39,60 @@ app.set('view engine', 'hbs');
 // dica: o handler desta função é bem simples - basta passar para o template
 //       os dados do arquivo data/jogadores.json
 app.set('views', 'server/views');
-	
+
 app.get('/', function(request, response) {
   response.render('index', {
   	players: db.players
   });
 });
 
+
 // EXERCÍCIO 3
 // definir rota para página de detalhes de um jogador --> renderizar a view
 // jogador, usando os dados do banco de dados "data/jogadores.json" e
 // "data/jogosPorJogador.json", assim como alguns campos calculados
-// dica: o handler desta função pode chegar a ter umas 15 linhas de código
-	
-app.get('/jogador/:numero_identificador/', function(request, response) {
+app.get('/jogador/:id/', function(request, response) {
+
+  // retorna detalhes do jogador
+  let player = _.find(db.players, function(el) {
+    return el.steamid === request.params.id;
+  });
+
+  // retorna jogos do jogador
+  let playerDetails = dbJogador[request.params.id];
+
+  // contabilizar a quantidade de jogos não jogados
+  playerDetails.notPlayed = _.where(playerDetails.games, {
+    playtime_forever: 0
+  }).length;
+
+  // ordenar por ordem decrescente de horas jogadas
+  playerDetails.games = _.sortBy(playerDetails.games, function(el) {
+    return -el.playtime_forever;
+  });
+
+  // retorna os 5 primeiros
+  playerDetails.games = _.first(playerDetails.games, 5);
+
+  // converter tempo jogado de minuto para hora
+  playerDetails.games = _.map(playerDetails.games, function(el) {
+    el.playtime_forever = Math.round(el.playtime_forever/60);
+    return el;
+  });
+
   response.render('jogador', {
-  	players: dbJogador.players,
-  	details: dbJogador[request.params.numero_identificador]
+    profile: player,
+    gameInfo: playerDetails,
+    favorite: playerDetails.games[0]
   });
 });
 
 
 // EXERCÍCIO 1
 // configurar para servir os arquivos estáticos da pasta "client"
-// dica: 1 linha de código
 app.use(express.static(__dirname + '/../client'));
 
 // abrir servidor na porta 3000
-// dica: 1-3 linhas de código
 let server = app.listen(3000, function () {
   console.log('Escutando em: http://localhost:3000');
 });
